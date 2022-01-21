ac574be98c156e3e42081a4b21789d63d4982425
Tristan Mckone <tristanmckone@gmail.com>
2022-01-21 15:39:48 -0500

    ice 2

--- Scripts/app.js
+++ Scripts/app.js
@@ -17,6 +17,46 @@
             location.href = "about.html";
         }
         );
+
+        // Step 1 get a reference an entry point (insertion point or deletion point)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+        
+        // Step 2 create an element(s) to insert
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3"> This is the Article Paragraph </p>`
+
+        //Step 3 configure new element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+
+        let FirstParagraphString = "This is";
+        // example of Template String
+        let SecondParagraphString = `${FirstParagraphString}the Main Paragraph`;
+
+        MainParagraph.textContent = SecondParagraphString;
+        Article.setAttribute("class", "container");
+
+        //Step 4 add / insert new element
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+        //Deletion example
+        //document.getElementById("ArticleParagraph").remove();
+
+        // Insert Before example
+
+        // let NewH1 = document.createElement("h1");
+        // NewH1.setAttribute("class", "display-1");
+
+        // MainContent.before(NewH1);
+
+        // NewH1.textContent = "Hello, World!";
+
+        
+
     }
 
     function DisplayProductPage()
