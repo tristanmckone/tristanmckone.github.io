e3d4dad4c56a8fad8f44604c4197af7986f75b96
Tristan Mckone <tristanmckone@gmail.com>
2022-01-28 16:43:16 -0500

    ICE 3 update

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,109 +1,163 @@
-// IIFE -- Immediately invoked function expression
-// Anonymous Self-Executing Function
+// IIFE -- Immediately Invoked Function Expression
+// AKA Anonymous Self-Executing Function
 (function()
 {
-    // named function option
-    /* function Start()
+
+    function DisplayAboutPage()
     {
-        console.log("App Started!!");
-    } */
+        console.log("About Us Page");
+    }
+
+    function DisplayProductsPage()
+    {
+        console.log("Products Page");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Services Page");
+    }
+
 
     function DisplayHomePage()
     {
-        console.log("Home Page")
+        console.log("Home Page");
+
         let AboutUsButton = document.getElementById("AboutUsButton");
+        
         AboutUsButton.addEventListener("click", function()
         {
+            // redirect to about page
             location.href = "about.html";
-        }
-        );
+        });
 
-        // Step 1 get a reference an entry point (insertion point or deletion point)
+        // Step 1 get a reference to an entry point(s) (insertion point / deletion point)
         let MainContent = document.getElementsByTagName("main")[0];
         let DocumentBody = document.body;
         
         // Step 2 create an element(s) to insert
         let MainParagraph = document.createElement("p");
         let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3"> This is the Article Paragraph </p>`
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
 
-        //Step 3 configure new element
+        // Step 3 configure new element
         MainParagraph.setAttribute("id", "MainParagraph");
         MainParagraph.setAttribute("class", "mt-3");
 
         let FirstParagraphString = "This is";
         // example of Template String
-        let SecondParagraphString = `${FirstParagraphString}the Main Paragraph`;
+        let SecondParagraphString = `${FirstParagraphString} the Main Paragraph`;
 
         MainParagraph.textContent = SecondParagraphString;
         Article.setAttribute("class", "container");
 
-        //Step 4 add / insert new element
+        // Step 4 add / insert new element
         MainContent.appendChild(MainParagraph);
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
 
-        //Deletion example
+
+        // Deletion example
         //document.getElementById("ArticleParagraph").remove();
 
         // Insert Before example
-
         // let NewH1 = document.createElement("h1");
         // NewH1.setAttribute("class", "display-1");
-
-        // MainContent.before(NewH1);
-
         // NewH1.textContent = "Hello, World!";
-
-        
-
-    }
-
-    function DisplayProductPage()
-    {
-        console.log("Products Page")
-    }
-
-    function DisplayServicesPage()
-    {
-        console.log("Services Page")
-    }
-
-    function DisplayAboutPage()
-    {
-        console.log("About Page")
+        // MainContent.before(NewH1);
     }
 
     function DisplayContactPage()
     {
-        console.log("Contact Page")
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function(event)
+        {
+            //event.preventDefault();
+
+            if(subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
     }
 
-    let Start = function()
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+
+        if(localStorage.length > 0) // check if localStorage has something in it 
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = "";
+
+            let keys = Object.keys(localStorage);
+
+            let index = 1;
+
+            //for every key in the keys collection loop
+            for(const key of keys)
+            {
+                let contactData = localStorage.getItem(key); // retrieve contact data from localStorage
+
+                let contact = new Contact(); // create an empty Contact Object
+                contact.deserialize(contactData);
+
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+                
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
+    }
+
+    // named function
+    function Start()
     {
         console.log("App Started!!");
 
-        switch(document.title)
+        switch (document.title) 
         {
-            case "Home":
+          case "Home":
             DisplayHomePage();
-                break;
-            case "Our Products":
-            DisplayProductPage();
-                break;
-            case "Our Services":
-            DisplayServicesPage();
-                break;
-            case "About Us":
-            DisplayAboutPage();
-                break;
-            case "Contact Us":
+            break;
+          case "Contact Us":
             DisplayContactPage();
-                break;
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Products":
+            DisplayProductsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
         }
-
-       
     }
+
     window.addEventListener("load", Start);
-    
+
 })();
---
+++ Scripts/contact.js
@@ -0,0 +1,66 @@
+class Contact
+{
+    // public properties (getters / setters)
+    get FullName()
+    {
+        return this.m_fullName;
+    }
+
+    set FullName(fullName)
+    {
+        this.m_fullName = fullName;
+    }
+
+    get ContactNumber()
+    {
+        return this.m_contactNumber;
+    }
+
+    set ContactNumber(contactNumber)
+    {
+        this.m_contactNumber = contactNumber;
+    }
+
+    get EmailAddress()
+    {
+        return this.m_emailAddress;
+    }
+
+    set EmailAddress(emailAddress)
+    {
+        this.m_emailAddress = emailAddress;
+    }
+
+    // constructor
+    constructor(fullName = "", contactNumber = "", emailAddress = "") // default string parameter
+    {
+        this.FullName =fullName;
+        this.ContactNumber = contactNumber;
+        this.EmailAddress = emailAddress;
+    }
+
+    // public methods
+    serialize()
+    {
+        if(this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== "")
+        {
+            return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
+        }
+        console.error("One or more properties of the Contact Object are missing or empty");
+        return null;
+    }
+
+    deserialize(data) // assume that the data object is a comma-separated list of properties
+    {
+        let propertyArray = data.split(",");
+        this.FullName = propertyArray[0];
+        this.ContactNumber = propertyArray[1];
+        this.EmailAddress = propertyArray[2];
+    }
+
+    // overridden methods
+    toString()
+    {
+        return `Full Name     : ${this.FullName}\nContact Number: ${this.ContactNumber}\nEmail Address : ${this.EmailAddress}`;
+    }
+}
